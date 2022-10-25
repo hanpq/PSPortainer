60a8ad3fed7066343047b4085b7c9ea02f692169
Hannes Palmquist <hannes.palmquist@outlook.com>
2022-10-25 10:34:23 +0200

    Updated module.tests.ps1 for module PSPortainer

--- tests/module/module.tests.ps1
+++ tests/module/module.tests.ps1
@@ -466,7 +466,7 @@ Describe -Name 'Foreach script file' -Tag 'Module' -Fixture {
         $MatchedLegacyCodeSnippets | Should -Be $null
     }
 
-    It -Name 'End Of Line Type: <FileName>' -Skip:($IsMacOS -eq $true) -TestCases $TestCases -Test {
+    It -Name 'End Of Line Type: <FileName>' -Skip:($IsMacOS -eq $true -or $IsLinux -eq $true) -TestCases $TestCases -Test {
         $Result = Test-FileEndOfLine -RawCode $CodeRaw
         ($Result -eq 'Windows' -or $Result -eq 'None') | Should -BeTrue
     }
